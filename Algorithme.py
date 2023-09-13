c0b869d38abcc34b5e5fd886e242f717569da6ca
Bastien-FONTAGNE <fontagne.bastien1@gmail.com>
2023-09-13 15:35:18 +0200

    Ajout de temperature min et max

--- Algorithme.py
+++ Algorithme.py
@@ -1,10 +1,18 @@
-Total = 0  #Initialisation de la variable "Total"
+#Initialisation de la variable "Total"
+Total = 0 
+
+#Initialisation des variables pour la température minimale et maximale
+temperature_min = float('inf')
+temperature_max = float('-inf')
 
 for i in range(31): #Répétez 31 fois
     
     temperature = float(input("Veuillez entrer une température: ")) #demande de rentrer la température à l'utilisateur
                         
     Total = Total + temperature #Ajoute la temperature à la variable totale
+
+    #Mets à hour la température minimale et maximale
+    if temperature < temperature_min
 
     Moyenne = Total/31 #Calcule la moyenne
 
