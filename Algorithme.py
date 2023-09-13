bc6720f1e8c0845c4a3e0531d53d3070d1381f3c
Bastien-FONTAGNE <fontagne.bastien1@gmail.com>
2023-09-13 15:25:42 +0200

    Rajoute les commentaires

--- Algorithme.py
+++ Algorithme.py
@@ -2,7 +2,7 @@ Total = 0  #Initialisation de la variable "Total"
 
 for i in range(31): #Répétez 31 fois
     
-    temperature = float(input("Veuillez entrer une température: ")) #demande de rentrée la température à l'utilisateur
+    temperature = float(input("Veuillez entrer une température: ")) #demande de rentrer la température à l'utilisateur
                         
     Total = Total + temperature #Ajoute la temperature à la variable totale
 
